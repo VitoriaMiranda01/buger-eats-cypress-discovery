507e645e0ec2a60d856805e8e84e9d02e07fe1a5
Vitória Miranda <DSNPACK@PAT3534.alterdata.matriz>
2022-09-13 17:07:40 -0300

    adicionando informações do dashboard do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    "projectId": "mn421k",
     "viewportWidth": 1440,
     "viewportHeight": 900,
     "baseUrl": "https://buger-eats-qa.vercel.app/",
